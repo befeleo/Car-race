a8ccb5f1c8018c6a3ec39d6efffdb60bbfecf44d
Your Name <you@example.com>
2025-12-26 13:34:02 +0300

    Fixed player car speed

--- PlayerCar.java
+++ PlayerCar.java
@@ -1,27 +1,36 @@
-import java.awt.*;
-
-// Todo: Java AWT Image sprit?
+import javafx.scene.image.Image;
+import javafx.scene.canvas.GraphicsContext;
 
 public class PlayerCar extends Car {
-
-    public PlayerCar(int x, int y, int speed, Image sprite) {
-        super(x, y, speed, sprite);
+    private double speedX = 0;
+    private double speedY = 0;
+    private final double MOVE_SPEED = 4.0; // Increased speed
+    
+    public PlayerCar(double x, double y, Image image) {
+        super(x, y, 0, image);
     }
-
-    public void moveLeft() {
-        x -= speed;
+    
+    @Override
+    public void update() {
+        x += speedX;
+        y += speedY;
     }
-
-    public void moveRight() {
-        x += speed;
+    
+    // Movement methods
+    public void moveUp() { speedX = MOVE_SPEED; }
+    public void moveDown() { speedX = -MOVE_SPEED; }
+    public void moveRight() { speedY = MOVE_SPEED; }
+    public void moveLeft() { speedY = -MOVE_SPEED; }
+    
+    // Stop methods - FIXED NAMES
+    public void stopVertical() { speedX = 0; }
+    public void stopHorizontal() { speedY = 0; }
+    
+    // Collision detection
+    public boolean collidesWith(Car other) {
+        return getRight() > other.getX() && 
+               getX() < other.getRight() &&
+               getBottom() > other.getY() && 
+               getY() < other.getBottom();
     }
-
-    public void moveUp() {
-        y -= speed;
-    }
-
-    public void moveDown() {
-        y += speed;
-    }
-
 }
