8a1bbab5629c7316fdd36e44c56940c8ffe25582
Betlihem <betlihem.degefu@aastustudent.edu>
2025-12-26 20:57:56 +0300

    Initial JavaFX Car Racing Project

---
+++ src/Game.java
@@ -0,0 +1,35 @@
+﻿import javafx.animation.AnimationTimer;
+import javafx.scene.layout.Pane;
+import javafx.scene.input.KeyCode;
+
+public class Game extends Pane {
+    private Car playerCar;
+    private Road road;
+    private RandomCars randomCars;
+
+    public Game() {
+        setPrefSize(400, 600);
+        playerCar = new Car(180, 500, this);
+        road = new Road(this);
+        randomCars = new RandomCars(this);
+
+        setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.LEFT) playerCar.moveLeft();
+            if (e.getCode() == KeyCode.RIGHT) playerCar.moveRight();
+        });
+
+        setOnKeyReleased(e -> playerCar.stop());
+    }
+
+    public void startGame() {
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                road.update();
+                playerCar.update();
+                randomCars.update();
+            }
+        };
+        timer.start();
+    }
+}
